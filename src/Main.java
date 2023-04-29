1469ebf738901aaa3119c43250991b121ca13a30
Babek Asadli <babekasadli@protonmail.com>
2023-04-29 21:58:36 +0400

    first

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    static char ghf;
+    public static void main(String[] args) {
+        System.out.println(ghf);
+        byte a =104, b=34, c=56;
+        byte d;
+        d = 5;
+        byte sum = (byte) (a+b+c);
+        System.out.println(sum);
+
+        String str = null;
+        System.out.println(str);
+        int bigger = (int) 346346903468346L;
+        long eded  = 930424234L;
+    }
+}
