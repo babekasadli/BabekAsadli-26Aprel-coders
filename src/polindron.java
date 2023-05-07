3163788fa62826fa898a915ce6aabeddbf2ced44
Babek Asadli <babekasadli@protonmail.com>
2023-05-07 14:24:07 +0400

    polindron

--- src/polindron.java
+++ src/polindron.java
@@ -1,10 +1,17 @@
 public class polindron {
     public static void main(String[] args) {
-        String a="Azerbaijan";
-        String b="abccba";
-        int[]arr=new int[a.length()];
-        for (int i = 0; i < a.length(); i++) {
-            arr[i]
+        String a="AbccbA";
+        char[] arr = a.toCharArray();
+        boolean result=false;
+        for (int i = 0,j= arr.length-1; i < arr.length&& j>0; i++,j--) {
+            if(arr[i]==arr[j]){
+                result=true;
+            }
+            else {
+                result = false;
+                break;
+            }
         }
+        System.out.println(result);
     }
 }
